e59e01f6a74860a18d98671dee038a227d4f00fc
Thiago Moura <thiago.moura@ccc.ufcg.edu.br>
2019-06-16 20:33:44 -0300

    merge done

--- index.js
+++ index.js
@@ -6,26 +6,28 @@ let players =  [];
 let statistics =  [];
 let result =  [];
 
-fs.createReadStream('players.csv')  
+fs.createReadStream('players_v2.csv')  
   .pipe(csv())
   .on('data', (row) => {
     players.push({...row});
   })
   .on('end', () => {
-    console.log('CSV file players.csv successfully processed');
+    console.log('CSV file players_v2.csv successfully processed');
 
-    fs.createReadStream('statistics.csv')  
+    fs.createReadStream('statistics_v2.csv')  
     .pipe(csv())
     .on('data', (row) => {
         statistics.push({...row});
     })
     .on('end', () => {
-      console.log('CSV file statistics.csv successfully processed');
+      console.log('CSV file statistics_v2.csv successfully processed');
       players.forEach(elem => {
-        const static = statistics.filter(s =>Number(s.Year) >=  2000).find(static => static.Player === elem.Player);
+        const static = statistics.find(static => static.Player.split('\\')[0] === elem.Name);
         if(static){
-            let obj = {nome: elem.Player, altura : elem.height, peso: elem.weight, livre: static['FT%'], goals: static['FG%']};
-            result.push(obj);
+          delete static.Player;
+          statistics = statistics.filter(elem => elem !== static);
+          const obj = {...elem, ...static};
+          result.push(obj);
         }
       });
       console.log(result);
@@ -38,13 +40,10 @@ fs.createReadStream('players.csv')
   const  writeCSV = (data) => {
     const csvWriter = createCsvWriter({  
       path: 'result.csv',
-      header: [
-        {id: 'name', title: 'name'},
-        {id: 'altura', title: 'altura'},
-        {id: 'peso', title: 'peso'},
-        {id: 'livre', title: 'livre'},
-        {id: 'goals', title: 'goals'},
-      ]
+      header: Object.keys(data[0]).reduce((accum = [], elem) => {
+        accum.push({id: elem, title: elem});
+        return accum;
+      }, []) 
     });
 
     csvWriter  
