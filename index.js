c825cc8ff82ac9cbbfd0a3ad16cc4b512df73398
Thiago Moura <thiago.moura@ccc.ufcg.edu.br>
2019-06-16 22:18:39 -0300

    done!

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const crawler = require("crawler-request");
 const async = require("async");
 
 let statistics = [];
-const cursor = "cm,";
+let valid = [];
+let result = [];
 
 fs.createReadStream("statistics_v2.csv")
   .pipe(csv())
@@ -15,45 +16,61 @@ fs.createReadStream("statistics_v2.csv")
   })
   .on("end", () => {
     console.log("CSV file statistics_v2.csv successfully processed");
-
-    async.eachLimit(
-      statistics.filter((elem, idx) => idx < 5),
-      10,
-      getDataLink,
-      err => {
-        if (err) throw err;
-
-        //console.log(statistics);
-        writeCSV(statistics);
+    //.filter((elem, idx) => idx < 100)
+    statistics.forEach(elem => {
+      const validKeys = Object.keys(elem).filter(item => !!elem[item]);
+      if (validKeys.length === Object.keys(elem).length) {
+        const obj = { ...elem };
+        valid.push(obj);
       }
-    );
+    });
+
+    async.eachLimit(valid, 5, getDataLink, err => {
+      if (err) throw err;
+
+      console.log(valid.length);
+      console.log(result.length);
+      writeCSV(result);
+    });
   });
 
 const getUrl = string => {
-  return `https://www.basketball-reference.com/players/a/${
-    string.Player.split("\\")[1]
-  }.html`;
+  const key = string.Player.split("\\")[1];
+  return `https://www.basketball-reference.com/players/${key.substring(
+    0,
+    1
+  )}/${key}.html`;
 };
 
 const getDataLink = (object, callback) => {
-  crawler(getUrl(object)).then(function(response) {
+  const url = getUrl(object);
+  console.log(url);
+  crawler(url).then(function(response) {
     if (response.text !== null) {
       setHeightAndWeight(response.text, object);
+    } else {
+      console.log("shit");
     }
     callback();
   });
 };
 
 const setHeightAndWeight = (text, object) => {
-  const beginHeight = text.indexOf('cm,');
-  const beginWeight = beginHeight + cursor.length;
+  object.Name = object.Player.split("\\")[0];
+  delete object.Player;
 
-  console.log(text.slice(beginHeight - 3, beginHeight + 8));
+  const height = text.match(/([0-9])+(cm)/)[0];
+  object.height = height.substring(0, height.length - 2);
+
+  const weight = text.match(/([0-9])+(kg)/)[0];
+  object.weight = weight.substring(0, weight.length - 2);
+
+  result.push(object);
 };
 
 const writeCSV = data => {
   const csvWriter = createCsvWriter({
-    path: "result.csv", 
+    path: "result.csv",
     header: Object.keys(data[0]).reduce((accum = [], elem) => {
       accum.push({ id: elem, title: elem });
       return accum;
